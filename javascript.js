17b6d8cfa4b11a5f945bc3cc2a7833fe05b1b06b
nicopriotto <nico.priotto@gmail.com>
2023-01-08 22:11:35 -0300

    Finish the project's first version

--- javascript.js
+++ javascript.js
@@ -26,15 +26,75 @@ function clearGrid(num) {
 
 const container = document.querySelector(".container");
 
+// Changing Colors while hovering
+function hoverColor(color) {
+    var box = document.querySelectorAll(".square");
+    box.forEach ((box) => {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = color;
+        })
+    })
+}
+
+// Changes color to the one selected
+const colorPen = document.querySelector(".color");
+    colorPen.addEventListener('input', () => {
+    hoverColor(colorPen.value);
+});
+
+// Changes background-color to the one selected
+const colorBackground = document.querySelector(".backgroundColor");
+colorBackground.addEventListener('input', () => {
+    const box = document.querySelectorAll('.square');
+    box.forEach((box) => {
+      box.style.backgroundColor = colorBackground.value;
+    });
+});
+
 // Slider to define grid size
 const slider = document.querySelector(".slider");
 createGrid(slider.value);
 const girdSize = document.querySelector(".gridSize")
 slider.oninput = function() {
     clearGrid(slider.value);
-    girdSize.textContent = "Select Grid Size: " + this.value + " x " + this.value;
+    girdSize.textContent = "Grid Size: " + this.value + " x " + this.value;
     createGrid(this.value);
+    hoverColor(colorPen.value);
 }
 
+// For when you start the page 
+hoverColor(colorPen.value);
 
+// Erase & Clear buttons
+const erase = document.querySelector(".erase");
+const clear = document.querySelector(".clear");
 
+erase.addEventListener('click', () => {
+    if (colorBackground.value != "#000000") {
+        hoverColor(colorBackground.value);
+    } else {
+        hoverColor("white")
+    }
+    erase.classList.add("bigButton");
+    setTimeout( () => {
+        erase.classList.remove("bigButton");
+    }, 300);
+})
+
+clear.addEventListener('click', () => {
+    if (colorBackground.value != "#000000") {
+        const box = document.querySelectorAll('.square');
+        box.forEach((box) => {
+        box.style.backgroundColor = colorBackground.value;
+    }); 
+    } else {
+        const box = document.querySelectorAll('.square');
+        box.forEach((box) => {
+        box.style.backgroundColor = "white";
+        });
+    }
+    clear.classList.add("bigButton");
+    setTimeout( () => {
+        clear.classList.remove("bigButton");
+    }, 300);
+})
